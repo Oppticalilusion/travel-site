3dfbdfec34c9b50e81f74762da7dc62166504235
Kevan Rayner <Kevanrayner@gmail.com>
2019-04-18 13:03:59 -0600

    First updates to the css of the site

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('default', function(){
+	console.log("Hooray - you created a Gulp task.");
+});
+
+gulp.task('html', function(){
+	console.log("Imagine something useful happening to your HTML");
+});
+
+gulp.task('styles', function(){
+	//console.log("Imagine Sass or PostCsSS tasks running here");
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function() {
+	
+	watch('./app/index.html', function() {
+		gulp.start('html');
+	});
+	
+	watch('./app/assets/styles/**/*.css', function() {
+		gulp.start('styles');
+	})
+	
+});
