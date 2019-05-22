1012b01a78722ef7db7f8d4a0ca4e27c45ef5adc
Kevan Rayner <Kevanrayner@gmail.com>
2019-05-22 11:27:43 -0600

    Added BrowserSync, reorganized file structure, and delt with gulp error handling

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('styles', function(){
+	//console.log("Imagine Sass or PostCsSS tasks running here");
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+		.on('error', function(errorInfo) {
+			console.log(errorInfo.toString());
+			this.emit('end');
+		})
+		.pipe(gulp.dest('./app/temp/styles'));
+});
