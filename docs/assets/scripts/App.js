6564d8782bf48cac2c8faf38192563941c46a088
Kevan Rayner <Kevanrayner@gmail.com>
2019-06-18 14:31:06 -0600

    Completed gulp build task.

---
+++ docs/assets/scripts/App.js
@@ -0,0 +1,48 @@
+/* Kevan Rayner 2019 */
+
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
+
+import $ from 'jquery';
+
+
+var mobileMenu = new MobileMenu();
+
+new RevealOnScroll($(".feature-item"), "85%"); //Our-features area
+new RevealOnScroll($(".testimonial"), "60%"); //Testimonials area
+
+var stickyHeader = new StickyHeader();
+
+var modal = new Modal();
+
+
+//var revealOnScroll = new RevealOnScroll();
+
+
+/* Examples used
+var $ = require('jquery');
+//var Person = require('./modules/Person');
+import Person from './modules/Person';
+
+class Adult extends Person {
+	payTaxes() {
+		console.log(this.name + " now owes &0 in taxes.");
+	}
+}
+
+//alert("ABC 321");
+
+ var john = new Person("John Doe", "blue");
+ john.greet();
+
+ var jane = new Adult("Jane Smith", "orange");
+ jane.greet();
+ jane.payTaxes();
+ 
+ $("h1").remove();
+
+//console.log(Person.exampleProperty);
+//Person.exampleFxn();
+*/
---
+++ docs/assets/scripts/Vendor.js
@@ -0,0 +1,3 @@
+import 'lazysizes';
+import 'picturefill'; //Allows responsive images to work in all browsers
+import '../../temp/scripts/modernizr';
